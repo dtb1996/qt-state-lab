ddd9bec36530a68f07cf976d0ac74b772bb33af3
Dillon Bellefeuille <dtb1996@yahoo.com>
2025-12-18 15:46:49 -0500

    Remove build directory from repo

--- build/Desktop_Qt_6_10_1-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_appQtStateLab_FILE /home/dillon/QtProjects/QtStateLab/build/Desktop_Qt_6_10_1-Debug/appQtStateLab)
-set(__QT_DEPLOY_TARGET_appQtStateLab_TYPE EXECUTABLE)
--- build/Desktop_Qt_6_10_1-Debug/.qt/appQtStateLab_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "appQtStateLab")
-set(working_dir "/home/dillon/QtProjects/QtStateLab")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/home/dillon/QtProjects/QtStateLab/build/Desktop_Qt_6_10_1-Debug/.qt/appQtStateLab_res.txt")
